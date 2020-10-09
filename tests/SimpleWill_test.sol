f31cbd6bb822138fb05ab97dca2293e6f721d725
Peter Lawson <lawsonpd@pm.me>
2020-10-09 15:57:52 -0500

    Add more tests for SimpleWill.

--- tests/SimpleWill_test.sol
+++ tests/SimpleWill_test.sol
@@ -5,52 +5,62 @@ import "../github/lawsonpd/will-and-trust-smart-contracts/SimpleWill.sol";
 
 // File name has to end with '_test.sol', this file can contain more than one testSuite contracts
 contract testSuite {
-    address payable acc0; // this will be beneficiary in testing
-    address acc1;
+    address payable acc0;
+    address payable acc1; // this will be beneficiary in tests
+    address payable acc2;
     
     SimpleWill will;
-
+    
     /// 'beforeAll' runs before all other tests
     function beforeEach() public {
         acc0 = payable(TestsAccounts.getAccount(0));
-        acc1 = TestsAccounts.getAccount(1); // This will be new owner in testChangeOwner
+        acc1 = payable(TestsAccounts.getAccount(1));
+        acc2 = payable(TestsAccounts.getAccount(2)); // This will be new owner in testChangeOwner
         
         will = new SimpleWill();
-        
-        // Add one beneficiary for each test
-        will.addBeneficiary(acc0);
+    }
+    
+    function testOwner() public {
+        Assert.equal(will.getOwner(), acc0, "Owner should be account acc0.");
+    }
+    
+    function testWillBalance() public {
+        address(will).call.value(10000 wei).gas(300000);
+        uint balance = will.getWillBalance();
+        Assert.equal(balance, 10000 wei, "Balance should at present be 10000.");
     }
     
     function beneficiaryExists() public {
+        will.addBeneficiary(acc1);
         address[] memory _beneficiaries = will.getBeneficiaries();
-        Assert.ok(_beneficiaries.length > 0, "There should be 1 beneficiary.");
+        Assert.greaterThan(uint(_beneficiaries.length), uint(0), "There should be 1 beneficiary.");
     }
     
     function testIsInactive() public {
-        Assert.ok(!will.willActivated, "Will should not be active at this point.");
+        Assert.ok(!will.isActive(), "Will should not be active at this point.");
     }
     
     function testBalanceAfterDeposit() public {
-        will.transfer(10000);
+        address(will).call.value(10000 wei).gas(300000);
         uint balance = will.getWillBalance();
         Assert.equal(balance, 10000, "Balance should at present be 10000.");
     }
     
     function testBeneficiaryBalance() public {
-        will.transfer(10000);
+        address(will).call.value(10000 wei).gas(300000);
         uint benefBalance = will.getBenefBalance();
-        Assert.equal(benefBalance, 10000, "Beneficiary balance should be 10000.");
+        Assert.equal(benefBalance, 10000, "Since there is 1 beneficiary, the balance thereof should be 10000.");
     }
     
     function testDepositAndActivateAndWithdraw() public {
-        will.transfer(10000);
+        address(will).call.value(10000 wei).gas(300000);
         will.activateWill();
         uint withdrawal = will.withdraw(); // Withdraw amount will be 0 but withdrawal should still be allowed
         Assert.equal(withdrawal, 0, "Withdrawal should be allowed and amount should be 0.");
     }
     
     function testChangeOwner() public {
-        will.changeOwner(acc1);
-        Assert.notEqual(address(this), will.owner, "Will owner should not be testing contract address.");
+        will.changeOwner(acc2);
+        Assert.ok(!will._isOwner(), "Will owner should not be testing contract address.");
     }
 }
